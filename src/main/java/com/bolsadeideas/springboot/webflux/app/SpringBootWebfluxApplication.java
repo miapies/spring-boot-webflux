39eb0bfd04fe7bbe4b4e274be5106225de5d64cc
Miguel Jiménez <miapies78@gmail.com>
2018-10-01 10:41:53 +0200

    Final de la sección

--- src/main/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -1,11 +1,14 @@
 package com.bolsadeideas.springboot.webflux.app;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 import com.bolsadeideas.springboot.webflux.app.models.dao.ProductoDao;
 import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
@@ -20,19 +23,28 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 	@Autowired
 	private ProductoDao dao;
 
+	@Autowired
+	private ReactiveMongoTemplate mongoTemplate;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebfluxApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		mongoTemplate.dropCollection("productos").subscribe();
+
 		Flux.just(new Producto("TV Panasonic Pantalla LCD", 456.89), new Producto("Sony Camara HD Digital", 177.89),
 				new Producto("Apple iPod", 46.89), new Producto("Sony Notebook", 846.89),
 				new Producto("Hewlett Packard Multifuncional", 200.89), new Producto("Bianchi Bicicleta", 70.89),
 				new Producto("HP Notebook Omen 17", 2500.89), new Producto("Mica Cómoda 5 Cajones", 150.89),
 				new Producto("TV Sony Bravia OLED 4k Ultra HD", 2255.89))
 				// .map(producto -> dao.save(producto))
-				.flatMap(producto -> dao.save(producto)).subscribe(
+				.flatMap(producto -> {
+					producto.setCreateAt(new Date());
+					return dao.save(producto);
+				}).subscribe(
 						producto -> log.info("Insert: " + producto.getId().concat(" ").concat(producto.getNombre())));
 	}
 }
